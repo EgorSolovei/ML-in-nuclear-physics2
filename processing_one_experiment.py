12acb363ad4e532285fbb6358279e5d9ab1231b9
Egor Linux <egor.egor-solowjew@yandex.ru>
2024-03-24 15:52:02 +0300

    Added angle files. Need to refactor

--- processing_one_experiment.py
+++ processing_one_experiment.py
@@ -8,6 +8,17 @@ from create_name import create_column_names
 
 def experiment_processing(borders, subrings, sensor_dist, step_angle, path_from, path_to,
                           start_file=1, end_file=100, mode_data="one_time"):
+
+    # создание пустого DataFrame с нужными колонками
+    sectors = int(2 * np.pi / step_angle)  # количество секторов на одном датчике
+    lst_col = create_column_names(len(sensor_dist), sectors, (len(subrings) - 1))
+
+    for dist in sensor_dist: # создание файлов с углами
+        theta = pd.Series([np.arctan(subring / dist) for subring in subrings])
+        theta.to_csv(path_to + f"/theta_angle.csv", index=False)
+        phi = pd.Series([i * step_angle for i in range(sectors)])
+        phi.to_csv(path_to + f"/phi_angle.csv", index=False)
+
     # на каждой итерации обрабатывается 1 файл и создаётся 1 файл готовых данных
     for file_i in range(start_file, end_file + 1):
         print(f"Начата обработка файла №{file_i}")
@@ -23,10 +34,6 @@ def experiment_processing(borders, subrings, sensor_dist, step_angle, path_from,
 
         # количество столкновений в файле (на случай, если их будет не постоянное количество)
         quan_event = data.query('mass == -1').agg({'mass': 'count'}).values[0]
-
-        # создание пустого DataFrame с нужными колонками
-        sectors = int(2 * np.pi / step_angle)  # количество секторов на одном датчике
-        lst_col = create_column_names(len(sensor_dist), sectors, (len(subrings) - 1))
         data_res = pd.DataFrame(columns=lst_col)  # результирующий DataFrame, который записывается в файл
 
         # создания файла 2000 событий\строк
